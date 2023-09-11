b03aaebcb2c3fcc7f05f85023677adaba800c096
krahil1996 <rahil393@gmail.com>
2023-09-11 19:12:27 +0530

    update files

--- Assignment - Quiz App [Major Assignment]/jscript.js
+++ Assignment - Quiz App [Major Assignment]/jscript.js
@@ -1,4 +1,22 @@
 // script for index page//
 // let username=[];
 //var username = document.getElementById("username");
-var username = 10;
+
+// var url_string = window.location;
+// var url = new URL(url_string);
+// var name = url.searchParams.get("name");
+// var btngroup = url.searchParams.get("btngroup");
+
+// document.getElementsByTagName("h3").innerHTML = btngroup;
+// document.getElementById('selected-catogery-by-user').innerHTML = btngroup;
+
+function smbt() {
+    
+    var user = document.getElementsByTagName("name").value;
+    var options = document.querySelector('input[name="btngroup"]:checked').value;
+    localStorage.setItem('Name', user);
+    localStorage.setItem('Options', options);
+    prompt(localStorage.getItem('Options'));
+    
+
+}
