e3e54dd1483bec94b71f35cc00301b1c393923c7
krahil1996 <rahil393@gmail.com>
2023-12-19 18:47:31 +0530

    update file

--- Signup and signIn Form/src/app.js
+++ Signup and signIn Form/src/app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app = express(); 
 const port = 3500;
 const path = require('path');
+const hbs = require('hbs');
+require("./db/conn"); 
+const registerUser= require('./models/register',)
+const static_path = path.join(__dirname, '../Public');
+const template_path = path.join(__dirname, '../Template/Views');
+const partials_path = path.join(__dirname, "../Template/Partials");
+hbs.registerPartials(partials_path)
 
-require('./db/conn'); 
 app.set ('view engine', 'hbs');
+app.set("views", template_path);
 
-app.get('/', (req, res) => {
-   res.render('index', path.join(__dirname, 'Views'))
+
+app.get('/', (req,res)=>{
+   res.render('index')
 })
+
+app.get("/login", (req, res) => {
+  res.render('login');
+});
+//4758027623  01-01-1999
+app.get("/register", (req, res) => {
+  res.render('register');
+});
 
 
 
