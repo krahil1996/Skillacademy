32e99d9c1f2ac01e449ea19020276c0b360f16ef
krahil1996 <rahil393@gmail.com>
2024-04-23 19:51:51 +0530

    update files

--- Hackathon2.0_Project/Todo_list/index.js
+++ Hackathon2.0_Project/Todo_list/index.js
@@ -1,57 +1,69 @@
 
-// Function to add a new task
 
-let inputbox = document.getElementById("input-box");
+
+let input = document.getElementById("input-box");
 let tasklist = document.getElementById("taskList");
 let emt_msg = document.getElementById("emt-msg");
 let instruction = document.getElementById('p-text');
+
+// Create Function to add a new task
 function addTask() {
-    if (inputbox.value.trim() === '') {
+    if (input.value.trim() === '') {
         // alert('Please enter the task name')
         setTimeout(() => {
             emt_msg.innerText = "Please Enter The Task"
         }, 500);
 
+        setInterval(() => {
+            emt_msg.innerText = "";
+        }, 5000);
     }
 
     else {
         var li = document.createElement("li");
         li.id = "list";
-        li.innerHTML = `${inputbox.value}<div><button type=smbt class=t-btn onclick="editTask(this)">Edit</button>
-        <button type=smbt class=t-btn onclick="deleteTask(this)">Delete</button>
+        li.innerHTML = `${input.value}<div><button type=smbt class=t-btn onclick="editTask(this)"><span>Edit</span></button>
+        <button type=smbt class=t-btn "><span>Delete</span></button>
     </div>`;
         tasklist.appendChild(li);
         instruction.innerText = ""
         instruction.style.borderColor = "#463333";
         instruction.style.borderStyle = "solid";
         instruction.style.borderWidth = "2px";
-
-    }
-
-    inputbox.value = "";
-
-    setInterval(() => {
         emt_msg.innerText = "";
-    }, 5000);
-}
+        input.value = "";
+        li.querySelector("span").addEventListener("click", deleteTask);
+        function deleteTask() {
+            li.remove(li);
+            // if (tasklist.innerText === "") {
+            //     instruction.innerText = "The list is empty";
+            //     instruction.style.border = "none";
+            // }
 
-function emtyMsg() {
-
-
-}
-
-
-deleteTask = () => {
-    let tasklist = document.getElementById("tasklist");
-    let list = document.getElementById("list");
-    tasklist.list.remove(list.parentNode);
-    if (tasklist.innerText === "") {
-        instruction.innerText = "The list is empty";
-        instruction.style.border = "none";
+        }
     }
 
-
 }
+
+
+
+// deleteTask = () => {
+//     let li = document.getElementById("list");
+//     li.remove(li);
+
+
+
+
+//     let tasklist = document.getElementById("tasklist");
+//     let list = document.getElementById("list");
+//     tasklist.list.remove(list.parentNode);
+//     if (tasklist.innerText === "") {
+//         instruction.innerText = "The list is empty";
+//         instruction.style.border = "none";
+//     }
+
+
+// }
 
 function editTask(list, newText) {
     // Find the task element by its ID
